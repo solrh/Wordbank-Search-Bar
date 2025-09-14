bcd8da1e74a66230d30678f9eed79c48d878779f
solrh <ewilso18@outlook.com>
2025-09-13 23:43:17 -0400

    Added a scripts folder for JS functionality; renamed main.js to script.js and added to scripts folder

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const searchBank = "swil\nWildMan";
+function searchBar(term, option){
+    if (option == "s"){ //Return words that start will the given pattern
+        term = `\\b${term}.*\\b`;
+    } else if (option == "e"){ //Return words that end with the given pattern
+        term = `\\b.*${term}\\b`;
+    } else { //Return words that contain the given pattern, position-agnostic
+        term = `\\b.*${term}.*\\b`;
+    }
+    term = new RegExp(term, "gi");
+    const results = searchBank.match(term);
+    return results ?? "No words found. Please try refining your search.";
+    
+}
